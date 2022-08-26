9debb6dedc5b41c46c07644bf0f938f19bf27502
gelin9527 <1376609520@qq.com>
2022-08-26 22:00:24 +0800

    更新redis lock 测试写法

--- redisLock_test.go
+++ redisLock_test.go
@@ -11,24 +11,26 @@ func Test_lockResult_Lock(t *testing.T) {
 	configure.SetDefault("Redis.default", "Server=localhost:6379,DB=15,Password=redis123,ConnectTimeout=600000,SyncTimeout=10000,ResponseTimeout=10000")
 	client := NewClient("default")
 	local := client.Lock.GetLocker("key_local", time.Duration(1000))
-	go control01(local)
+	control01(local)
 	go control02(local)
 	time.Sleep(time.Duration(100))
 }
 
 func control01(local lockResult) {
+	if !local.TryLock() {
+		fmt.Println("-----01加锁失败")
+	}
+	defer local.ReleaseLock()
 	for i := 0; i < 10; i++ {
-		if local.TryLock() {
-			fmt.Println("-----值：a", i)
-			local.ReleaseLock()
-		}
+		fmt.Println("-----值：a", i)
 	}
 }
 func control02(local lockResult) {
+	if !local.TryLock() {
+		fmt.Println("-----02加锁失败")
+	}
+	defer local.ReleaseLock()
 	for i := 0; i < 10; i++ {
-		if local.TryLock() {
-			fmt.Println("-----值：b", i)
-			local.ReleaseLock()
-		}
+		fmt.Println("-----值：b", i)
 	}
 }
