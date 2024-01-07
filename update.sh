5dcc5ba22466154d09034d8c2797ea77bd5d7786
steden <1470804@qq.com>
2024-01-07 19:06:57 +0800

    移除文件

--- update.sh
+++
@@ -1,5 +0,0 @@
-go get github.com/farseer-go/cache
-go get github.com/farseer-go/collections
-go get github.com/farseer-go/fs
-go get github.com/go-redis/redis/v8
-go get github.com/stretchr/testify
