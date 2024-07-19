de85e3d17a46890546bb72c2d37c72af0d7de23f
steden <1470804@qq.com>
2024-07-19 23:18:33 +0800

    添加日志

--- redisPipeline.go
+++ redisPipeline.go
@@ -21,7 +21,10 @@ func (receiver *redisPipeline) Transaction(executeFn func()) error {
 	txPipeline := receiver.GetClient().TxPipeline()
 	flog.Infof("使用事务：%d", routine.Goid())
 	routineRedisClient.Set(txPipeline)
-	defer func() { routineRedisClient.Remove() }()
+	defer func() {
+		routineRedisClient.Remove()
+		flog.Infof("移除事务：%d", routine.Goid())
+	}()
 	executeFn()
 	_, err = txPipeline.Exec(context.Background())
 	return err
@@ -36,7 +39,10 @@ func (receiver *redisPipeline) Pipeline(executeFn func()) (PipelineCmder, error)
 	txPipeline := receiver.GetClient().Pipeline()
 	flog.Infof("使用管道：%d", routine.Goid())
 	routineRedisClient.Set(txPipeline)
-	defer func() { routineRedisClient.Remove() }()
+	defer func() {
+		routineRedisClient.Remove()
+		flog.Infof("移除管道：%d", routine.Goid())
+	}()
 
 	executeFn()
 	result, err := txPipeline.Exec(context.Background())
