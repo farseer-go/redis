3ef6a94d780510042720896f2b5c840971859e0e
steden <1470804@qq.com>
2024-10-24 18:52:26 +0800

    新增对map的支持

--- cmder.go
+++ cmder.go
@@ -2,28 +2,37 @@ package redis
 
 import (
 	"encoding/json"
+	"reflect"
+
+	"github.com/farseer-go/fs/parse"
 	"github.com/farseer-go/fs/types"
 	"github.com/go-redis/redis/v8"
-	"reflect"
 )
 
 type PipelineCmder struct {
 	cmder []redis.Cmder
 }
 
-func (receiver PipelineCmder) ToList(sliceOrList any) {
+func (receiver PipelineCmder) Fill(sliceOrList any) {
 	sliceOrListVal := reflect.ValueOf(sliceOrList).Elem()
 
 	// 切片类型
-	if sliceOrListType, isSlice := types.IsSlice(sliceOrListVal); isSlice {
-		value := reflect.MakeSlice(sliceOrListType, 0, 0)
+	if sliceType, isSlice := types.IsSlice(sliceOrListVal); isSlice {
+		itemType := sliceType.Elem()
+		value := reflect.MakeSlice(sliceType, 0, 0)
+
 		for _, cmder := range receiver.cmder {
 			switch cmderType := cmder.(type) {
 			case *redis.StringCmd:
-				item := reflect.New(sliceOrListType.Elem()).Interface()
+				item := reflect.New(itemType).Interface()
 				if jsonVal := cmderType.Val(); jsonVal != "" {
 					_ = json.Unmarshal([]byte(jsonVal), item)
 					value = reflect.Append(value, reflect.ValueOf(item).Elem())
+				}
+			case *redis.StringStringMapCmd:
+				jsonVal := cmderType.Val()
+				for _, v := range jsonVal {
+					value = reflect.Append(value, reflect.ValueOf(parse.ConvertValue(v, itemType)))
 				}
 			}
 		}
@@ -31,10 +40,11 @@ func (receiver PipelineCmder) ToList(sliceOrList any) {
 		return
 	}
 
-	if sliceOrListType, isList := types.IsList(sliceOrListVal); isList {
-		itemType := types.GetListItemType(sliceOrListType)
-		// 初始化
-		value := types.ListNew(sliceOrListType)
+	// List类型
+	if listType, isList := types.IsList(sliceOrListVal); isList {
+		itemType := types.GetListItemType(listType)
+		value := types.ListNew(listType)
+
 		for _, cmder := range receiver.cmder {
 			switch cmderType := cmder.(type) {
 			case *redis.StringCmd:
@@ -43,11 +53,31 @@ func (receiver PipelineCmder) ToList(sliceOrList any) {
 					_ = json.Unmarshal([]byte(jsonVal), item)
 					types.ListAdd(value, item)
 				}
+			case *redis.StringStringMapCmd:
+				jsonVal := cmderType.Val()
+				for _, v := range jsonVal {
+					types.ListAdd(value, parse.ConvertValue(v, itemType))
+				}
 			}
 		}
 		sliceOrListVal.Set(value.Elem())
 		return
 	}
 
-	panic("sliceOrList入参必须为切片或collections.List类型")
+	// map类型
+	if mapType, isList := types.IsMap(sliceOrListVal); isList {
+		// make.... var m map[k]v = make(map[k]v)
+		sliceOrListVal.Set(reflect.MakeMap(mapType))
+		for _, cmder := range receiver.cmder {
+			switch cmderType := cmder.(type) {
+			case *redis.StringStringMapCmd:
+				jsonVal := cmderType.Val()
+				for k, v := range jsonVal {
+					sliceOrListVal.SetMapIndex(reflect.ValueOf(k), reflect.ValueOf(v))
+				}
+			}
+		}
+		return
+	}
+	panic("sliceOrList入参必须为切片、collections.List类型、map类型")
 }
