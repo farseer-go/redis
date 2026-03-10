16d217844c235fc9b3611fd55b17a4936e084416
steden <steden@fsgit.com>
2026-03-10 22:21:01 +0800

    修改配置文件

--- setProfiles.go
+++ setProfiles.go
@@ -1,15 +1,16 @@
 package redis
 
 import (
+	"reflect"
+
 	"github.com/farseer-go/cache"
 	"github.com/farseer-go/fs/exception"
-	"reflect"
 )
 
 // SetProfiles 设置缓存（集合）
 // key：这批数据的key，用于区分不同数据集合。
 // uniqueField：数据集合中，用于区分Item的唯一值的字段名称（主键）
-// redisConfigName：farseer.yaml的Redis.xx配置名称
+// redisConfigName：config.yaml的Redis.xx配置名称
 // ops：选项
 func SetProfiles[TEntity any](key string, uniqueField string, redisConfigName string, ops ...cache.Option) cache.ICacheManage[TEntity] {
 	if uniqueField == "" {
